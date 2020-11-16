abca11c62de37ca50b6bdd5bc27eb8786a2cbe5a
Keian Rao <keian.rao@gmail.com>
2020-11-16 18:38:51 -0500

    4: Added test data

--- 4/lookup_race.py
+++ 4/lookup_race.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 
-##  %%  ##  %%  ##  %%  ##  %%  ##  %%  ##  %%  ##
-
+import random
 import timeit
 
-def something():
-    string = "";
-    for number in range(1, 10):
-        string += str(number);
-    return string;
+def pop_random(a_seq):
+    choice = random.choice(a_seq);
+    a_seq.remove(choice);
+    return choice;
 
 def main():
-    print(timeit.timeit(something));
+    test_data = [
+        ("key", "value"),
+        ("name", "value"),
+        ("compiled", "down"),
+        ("interpreted", "down"),
+        ("transpiled", "up"),
+        ("quotations", "prose"),
+        ("crochets", "music"),
+        ("pronunciation", "pronounce"),
+        ("enunciation", "enunciate"),
+        ("asia", "large"),
+        ("europe", "large"),
+        ("svalbard", "large"),
+        ("studio bedroom", "large"),
+        ("お言葉", "for good measure")
+    ];
 
 if __name__ == "__main__": main();
