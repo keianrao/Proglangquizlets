984a40707d4ca424231f6de7198b96d04ba303bf
Keian Rao <keian.rao@gmail.com>
2020-11-16 19:00:47 -0500

    4: Finished basic Python 3 impl.

--- 4/lookup_race.py
+++ 4/lookup_race.py
@@ -9,6 +9,7 @@ def pop_random(a_seq):
     return choice;
 
 def main():
+    # Initialise test data for this run
     test_data = [
         ("key", "value"),
         ("name", "value"),
@@ -25,5 +26,36 @@ def main():
         ("studio bedroom", "large"),
         ("お言葉", "for good measure")
     ];
+
+    # Initialise hash dictionary and associative list
+    hash_dict = dict()
+    assoc_list = []
+
+    # Okay, start the test.
+    random.seed();
+    while len(test_data) > 0:
+        # Pop random pair from test data and add into both
+        (key, value) = pop_random(test_data);
+        hash_dict[key] = value;
+        assoc_list.append((key, value)); # sic.
+
+        def hash_dict_lookup():
+            if key not in hash_dict:
+                assert False;
+        def assoc_list_lookup():
+            for (curr_key, _) in assoc_list:
+                if curr_key == key: return;
+            assert False;
+
+        # Measure lookup runtime
+        hash_dict_time = timeit.timeit(hash_dict_lookup, number = 5);
+        assoc_list_time = timeit.timeit(assoc_list_lookup, number = 5);
+
+        if hash_dict_time < assoc_list_time:
+            print("Hash dict wins at size " + str(len(hash_dict)));
+            print("hash dict " + str(hash_dict_time) + " sec");
+            print("assoc list " + str(assoc_list_time) + " sec");
+            print(hash_dict);
+            return;
 
 if __name__ == "__main__": main();
